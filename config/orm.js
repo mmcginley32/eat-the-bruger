372373f4ad0745d32c8885e699b50ae05d797f4a
Mason McGinley <masonmcginley32@gmail.com>
2020-10-25 14:27:37 -0600

    config added

---
+++ config/orm.js
@@ -0,0 +1,75 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+// Object for all our SQL statement functions.
+var orm = {
+  selectAll: function(table, cb){
+    const query = `SELECT * FROM ${table};`
+    connection.query(query, function(err, res){
+      if (err) throw err;
+
+      cb(res);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  
+
+  updateOne: function(table, objColVals, condition, cb){
+    const query = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition};`
+    connection.query(query, function(err, res){
+      if (err) throw err;
+
+      cb(res);
+    });
+  },  
+};
+
+module.exports = orm;
